89810817d5e3f88c6cff237ad5c20a2818fcf556
Johnik <you@example.com>
2024-04-20 11:26:57 +0300

    test

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private Long id;
-    @NotBlank
+    @NotBlank(message = "не должно быть пустым")
     private String name;
     @Size(max = 200)
     private String description;
