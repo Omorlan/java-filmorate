01f7a4798e45e6dac3f47d4508130ac990f6b610
Evgeniy <cokrat@yandex.ru>
2024-04-25 22:20:16 +0300

    fix: Убрать явное определение бинов fix: Убрать неиспользуемые геттеры и сетеры fix: Исправить переменную пути fix: добавить final полям контроллеров

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -3,6 +3,10 @@ package ru.yandex.practicum.filmorate.model;
 public class ErrorResponse {
     private String error;
 
+    public String getError() {
+        return error;
+    }
+
     public ErrorResponse(String error) {
         this.error = error;
     }
