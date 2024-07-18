774139afb688694118da0ac9d29d9ff530a4fe54
Johnik <you@example.com>
2024-07-19 01:21:52 +0300

    fix: branch test

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorageDb.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorageDb.java
@@ -201,7 +201,6 @@ public class ReviewStorageDb implements ReviewStorage {
         if (userCount == null || userCount == 0) {
             throw new NotFoundException("User with id " + userId + " does not exist");
         }
-
         if (filmCount == null || filmCount == 0) {
             throw new NotFoundException("Film with id " + filmId + " does not exist");
         }
