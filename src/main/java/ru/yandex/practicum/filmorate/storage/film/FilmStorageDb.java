365e47caec4399d87331d9b1d947c88f4aba8820
Paul Value <paulvalue@gmail.com>
2024-07-19 15:46:14 +0300

    Исправить замечания

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorageDb.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorageDb.java
@@ -19,7 +19,12 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
